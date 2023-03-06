1000d028eb7902c568bd36850f0b9670cc069656
gemilank13 <39510051+gemilank13@users.noreply.github.com>
2023-03-06 16:27:03 +0700

    Add files via upload

---
+++ project_2_board/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Board Monitoring Project",
+
+    'summary': """
+       """,
+
+    'description': """
+        
+    """,
+
+    'author': "Geger Gemilank",
+    'website': ".",
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    'depends': ['project_2','board'],
+
+    'data': [
+               # 'security/ir.model.access.csv',
+               # 'security/security.xml',
+               'views/dashboard.xml',
+               # 'views/sequence.xml',
+               # 'views/billing.xml',
+               # 'views/config.xml',
+               # 'views/inherit_user.xml',
+            ],
+    
+    'demo': [],
+}
